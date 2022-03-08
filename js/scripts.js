fbcc66111a87feb72e9da0e7f0a4fccf700420a0
Jonathan Cheng <jcheng461@gmail.com>
2022-03-09 00:22:49 +0900

    fix refactor form to use arrays and loops to display text to user --- earlier, somehow I thought it worked, but actually did not. Now it works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
   $("#letterForm").submit(function (event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    const date1Input = $("input#date1").val();
-    const date2Input = $("input#date2").val();
-    const date3Input = $("input#date3").val();
-    const person2Input = $("input#person2").val();
+    const person1Input = $("input#0").val();
+    const date1Input = $("input#1").val();
+    const date2Input = $("input#2").val();
+    const date3Input = $("input#3").val();
+    const person2Input = $("input#4").val();
+
+    console.log(person1Input);
+    console.log(date1Input);
+    console.log(date2Input);
+    console.log(date3Input);
+    console.log(person2Input);
 
     const userInput = [
       person1Input,
@@ -16,9 +22,17 @@ $(document).ready(function () {
     ];
 
     // Counter keeps track of which type-in form to grab text from.
+    letterTemplateLocation = [
+      ".person1",
+      ".date1",
+      ".date2",
+      ".date3",
+      ".person2",
+    ];
+
     let counter = 0;
-    userInput.forEach(function (entry) {
-      $("counter").text(entry);
+    userInput.forEach(function (input) {
+      $(letterTemplateLocation[counter]).text(input);
       counter += 1;
     });
 
